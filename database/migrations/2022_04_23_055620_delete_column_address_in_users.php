1092aead671c43d5403bb13042793fdb728ad450
mHanash <zenasmomonzo@gmail.com>
2022-04-23 07:16:50 +0100

    add differents columns in users table and create addresses and phones tables

---
+++ database/migrations/2022_04_23_055620_delete_column_address_in_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('address');
+            $table->dropColumn('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
+    }
+};
