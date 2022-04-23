1ffc25f9131c3a3f20082de7bbe5d7f1f935ef80
mHanash <zenasmomonzo@gmail.com>
2022-04-23 08:32:24 +0100

    create controllers and routes for all models

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Person $person)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Person $person)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Person $person)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\CreditCardController;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\PhoneController;
+use App\Http\Controllers\TransfertController;
+use App\Http\Controllers\UserController;
+use FastRoute\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,6 +21,43 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+// All routes for Models/Address
+$router->get('/address',[AddressController::class,'index']);
+$router->post('/address',[AddressController::class,'store']);
+$router->get('/address/{id}',[AddressController::class,'show']);
+$router->post('/address/update',[AddressController::class,'update']);
+$router->delete('/address/{id}',[AddressController::class,'destroy']);
+// All routes for Models/CreditCard
+$router->get('/credit_card',[CreditCardController::class,'index']);
+$router->post('/credit_card',[CreditCardController::class,'store']);
+$router->get('/credit_card/{id}',[CreditCardController::class,'show']);
+$router->post('/credit_card/update',[CreditCardController::class,'update']);
+$router->delete('/credit_card/{id}',[CreditCardController::class,'destroy']);
+// All routes for Models/Person
+$router->get('/person',[PersonController::class,'index']);
+$router->post('/person',[PersonController::class,'store']);
+$router->get('/person/{id}',[PersonController::class,'show']);
+$router->post('/person/update',[PersonController::class,'update']);
+$router->delete('/person/{id}',[PersonController::class,'destroy']);
+// All routes for Models/Phone
+$router->get('/phone',[PhoneController::class,'index']);
+$router->post('/phone',[PhoneController::class,'store']);
+$router->get('/phone/{id}',[PhoneController::class,'show']);
+$router->post('/phone/update',[PhoneController::class,'update']);
+$router->delete('/phone/{id}',[PhoneController::class,'destroy']);
+// All routes for Models/Transfert
+$router->get('/transfert',[TransfertController::class,'index']);
+$router->post('/transfert',[TransfertController::class,'store']);
+$router->get('/transfert/{id}',[TransfertController::class,'show']);
+$router->post('/transfert/update',[TransfertController::class,'update']);
+$router->delete('/transfert/{id}',[TransfertController::class,'destroy']);
+// All routes for Models/User
+$router->get('/user',[UserController::class,'index']);
+$router->post('/user',[UserController::class,'store']);
+$router->get('/user/{id}',[UserController::class,'show']);
+$router->post('/user/update',[UserController::class,'update']);
+$router->delete('/user/{id}',[UserController::class,'destroy']);
